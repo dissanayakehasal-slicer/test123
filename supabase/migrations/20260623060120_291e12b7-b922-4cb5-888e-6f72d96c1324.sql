beb16e7157e646200b944f3b2b2f0a5507104385
GMS Developer <dev@geethmunasinghe.lk>
2026-06-23 13:00:14 +0530

    seed: set launch_at to 2026-06-23 13:10 GMT+5:30

--- supabase/migrations/20260623060120_291e12b7-b922-4cb5-888e-6f72d96c1324.sql
+++ supabase/migrations/20260623060120_291e12b7-b922-4cb5-888e-6f72d96c1324.sql
@@ -15,7 +15,7 @@ CREATE POLICY "Anyone can read launch config"
   ON public.launch_config FOR SELECT
   USING (true);
 
--- Seed with a fixed launch time: 2026-06-23 13:00 GMT+5:30
+-- Seed with a fixed launch time: 2026-06-23 13:10 GMT+5:30
 INSERT INTO public.launch_config (id, launch_at)
-VALUES (1, TIMESTAMPTZ '2026-06-23 13:00:00+05:30')
+VALUES (1, TIMESTAMPTZ '2026-06-23 13:10:00+05:30')
 ON CONFLICT (id) DO UPDATE SET launch_at = EXCLUDED.launch_at;
