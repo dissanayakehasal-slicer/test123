341d9e23a8c01de9d21808536a4fc78c4bdc3254
GMS Developer <dev@geethmunasinghe.lk>
2026-06-23 17:28:43 +0530

    chore: update launch time to 17:30

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,7 +20,7 @@ export const Route = createFileRoute("/")({
 
 function useServerSyncedNow() {
   const DEFAULT_LAUNCH_AT = useMemo(
-    () => new Date("2026-06-23T11:46:00.000Z").getTime(),
+    () => new Date("2026-06-23T12:00:00.000Z").getTime(),
     [],
   );
 
